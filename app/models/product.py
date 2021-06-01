cc0630c84a61622f807984be9670f795fcf71b17
Tran <tranle97@gmail.com>
2021-05-31 21:49:43 -0500

    Add user_id to products model

--- app/models/product.py
+++ app/models/product.py
@@ -8,11 +8,13 @@ class Product(db.Model):
   brand_name = db.Column(db.String, nullable=False)
   skincare_step = db.Column(db.String)
   target = db.Column(db.String)
-  time_of_use = db.Column(db.String, nullable=False, default='AM, PM')
+  am_use = db.Column(db.Boolean, default=False)
+  pm_use = db.Column(db.Boolean, default=False)
   description = db.Column(db.Text)
   precautions = db.Column(db.String, default='N/A')
   ingredients = db.Column(db.Text, default='N/A')
   img_url = db.Column(db.Text)
+  user_id = db.Column(db.Integer, default=None)
 
   routine_products = db.relationship('RoutineProducts', back_populates='products')
 
@@ -23,9 +25,11 @@ class Product(db.Model):
       'brand_name': self.brand_name,
       'skincare_step': self.skincare_step,
       'target': self.target,
-      'time_of_use': self.time_of_use,
+      'am_use': self.am_use,
+      'pm_use': self.pm_use,
       'description': self.description,
       'precautions': self.precautions,
       'ingredients': self.ingredients,
-      'img_url': self.img_url
+      'img_url': self.img_url,
+      'user_id': self.user_id
     }
