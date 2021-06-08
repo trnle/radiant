884d5256ebc77652d483707827670d5624dbfae6
Tran <tranle97@gmail.com>
2021-06-08 17:14:56 -0500

    .

--- react-app/src/components/Entry/index.js
+++ react-app/src/components/Entry/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams, useHistory } from 'react-router-dom';
+import { useParams, useHistory, NavLink } from 'react-router-dom';
 import { getEntry, deleteOneEntry } from '../../store/entries';
 import entryImg from '../../images/entryImg.png';
 import './Entry.css';
@@ -13,25 +13,28 @@ const Entry = () => {
   const user = useSelector(state => state.session.user);
   const entry = useSelector(state => state.entries.oneEntry);
   
-  document.title = `${entry.created_at} | Radiant`;
-  document.body.style = 'background-color: #FFFFFF';
   
   useEffect(() => {
     dispatch(getEntry(id));
   }, [dispatch, id])
-
+  
   const deleteEntry = async e => {
     e.preventDefault();
     await dispatch(deleteOneEntry(id));
     history.push('/journal');
   }
-
+  
   if (!entry) return null;
-
+  document.title = `${entry.created_at} | Radiant`;
+  document.body.style = 'background-color: #FFFFFF';
+  
   if (!entry.img_url) entry.img_url = entryImg;
 
   return (
     <div id='entry-page'>
+      <div id='nav-entries'>
+        <NavLink to='/journal'>Back to Journal</NavLink>
+      </div>
       <div id='entry-summary'>
         <h4>{entry.created_at}</h4>
         {entry.img_url && <img src={entry.img_url} alt="Skin progress" height='70' />}
