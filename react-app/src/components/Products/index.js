620a81831ddb1ef1706f332fb5f5c7d22d3f2946
Tran <tranle97@gmail.com>
2021-06-08 22:31:58 -0500

    Filter by skincare step or user products

--- react-app/src/components/Products/index.js
+++ react-app/src/components/Products/index.js
@@ -12,46 +12,60 @@ const Products = () => {
   let products = useSelector(state => state.products.allProducts);
   const dispatch = useDispatch();
   const history = useHistory();
-
+  const user = useSelector(state => state.session.user);
   const [filter, setFilter] = useState('')
-  const [filteredProducts, setFilteredProducts] = useState([])
-  console.log(filter, 'fdsfj=filter-------')
+
   useEffect(() => {
     dispatch(getProducts())
   }, [dispatch])
+
+
+  let filteredProducts;
+  if (filter === 'My Products') {
+    filteredProducts = Object.values(products).filter(product => product.user_id === user.id)
+  } else {
+    filteredProducts = Object.values(products).filter(product => product.skincare_step === filter)
+  }
+
+  if (filteredProducts.length === 0) {
+    filteredProducts = products;
+  } 
   
   if (!products) return null;
 
   return (
     <div id='products-page'>
-      <h1>Products</h1>
-      <CreateProductModal />
       <div>
-        <label>Filter</label>
-        <select id="filter" onChange={e => setFilter(e.target.value)}>
-          <option value=""></option>
-          <option value="Cleanse">Cleanse</option>
-          <option value="Treat">Treat</option>
-          <option value="Nourish">Nourish</option>
-          <option value="Moisturize">Moisturize</option>
-          <option value="Protect">Protect</option>
-          <option value="Other">Other</option>
-        </select>
-      </div>
-      <div id='product-grid'>
-        {Object.values(products).map(product => (
-          <div className='product-container' key={product.id} onClick={e => { e.preventDefault(); history.push(`/products/${product.id}`) }}>
-            <div id='products-imgs'>
-              <img src={product.img_url} alt={product.product_name} />
+        <h1>Products</h1>
+        <CreateProductModal />
+        <div id='filter-container'>
+          <label>Filter</label>
+          <select id="filter" onChange={e => setFilter(e.target.value)}>
+            <option value="">All</option>
+            <option value="Cleanse">Cleanse</option>
+            <option value="Treat">Treat</option>
+            <option value="Nourish">Nourish</option>
+            <option value="Moisturize">Moisturize</option>
+            <option value="Protect">Protect</option>
+            <option value="Other">Other</option>
+            <option value="My Products">My Products</option>
+          </select>
+        </div>
+        <div id='product-grid'>
+          {Object.values(filteredProducts).map(product => (
+            <div className='product-container' key={product.id} onClick={e => { e.preventDefault(); history.push(`/products/${product.id}`) }}>
+              <div id='products-imgs'>
+                <img src={product.img_url} alt={product.product_name} />
+              </div>
+              <div id='products-overview'>
+                <NavLink to={`/products/${product.id}`}>{product.product_name}</NavLink>
+                <h4>{product.brand_name}</h4>
+                <p>{product.description}</p>
+              </div>
             </div>
-            <div id='products-overview'>
-              <NavLink to={`/products/${product.id}`}>{product.product_name}</NavLink>
-              <h4>{product.brand_name}</h4>
-              <p>{product.description}</p>
-            </div>
-          </div>
-        ))}
+          ))}
 
+        </div>
       </div>
     </div>
   )
