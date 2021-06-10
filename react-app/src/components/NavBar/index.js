6f5dde84ed325e975011c0099c0987d21a3866ab
Tran <tranle97@gmail.com>
2021-06-10 15:02:32 -0500

    Nav bar fix and products image

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../Auth/LogoutButton';
-// import logo from '../../images/radiant-sparkles.png';
 import SearchBar from './SearchBar';
 import './NavBar.css';
 
