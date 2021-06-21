eeee797b3df9f0261c1a37ab4b8cf3ae685fbcdb
Tran <tranle97@gmail.com>
2021-06-20 23:55:02 -0500

    Add target _blank to links to open new tab

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -3,23 +3,23 @@ import './Footer.css';
 
 const Footer = () => {
   return (
-      <div id='footer-container'>
-        <div id='by-line'>
-          Developed by Tran Le
+    <div id='footer-container'>
+      <div id='by-line'>
+        Developed by Tran Le
         <span role='img' aria-label='bee'> &#128029;</span>
-        </div>
-        <div id='footer-links'>
-          <a href='http://tranle.dev/'>
-            <img src='https://img.icons8.com/dusk/64/000000/web-design.png' alt="Icon linking to Tran's portfolio website" height='30' />
-          </a>
-          <a href='https://github.com/trnle/radiant'>
-            <img src='https://img.icons8.com/dusk/64/000000/github.png' alt="Icon linking to Tran's GitHub" height='30' />
-          </a>
-          <a href='https://www.linkedin.com/in/trnle/'>
-            <img src='https://img.icons8.com/dusk/64/000000/linkedin.png' alt="Icon linking to Tran's LinkedIn" height='30' />
-          </a>
-        </div>
       </div>
+      <div id='footer-links'>
+        <a href='http://tranle.dev/' target='_blank' rel='noreferrer'>
+          <img src='https://img.icons8.com/dusk/64/000000/web-design.png' alt="Icon linking to Tran's portfolio website" height='30' />
+        </a>
+        <a href='https://github.com/trnle/radiant' target='_blank' rel='noreferrer'>
+          <img src='https://img.icons8.com/dusk/64/000000/github.png' alt="Icon linking to Tran's GitHub" height='30' />
+        </a>
+        <a href='https://www.linkedin.com/in/trnle/' target='_blank' rel='noreferrer'>
+          <img src='https://img.icons8.com/dusk/64/000000/linkedin.png' alt="Icon linking to Tran's LinkedIn" height='30' />
+        </a>
+      </div>
+    </div>
   )
 }
 
