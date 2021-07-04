7731d05f85de376d7e01203d0f21aa546e687acd
Tran <tranle97@gmail.com>
2021-07-03 21:43:34 -0500

    Add noopener to footer links for security

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -9,13 +9,13 @@ const Footer = () => {
         <span role='img' aria-label='bee'> &#128029;</span>
       </div>
       <div id='footer-links'>
-        <a href='http://tranle.dev/' target='_blank' rel='noreferrer'>
+        <a href='http://tranle.dev/' target='_blank' rel='noopener noreferrer'>
           <img src='https://img.icons8.com/dusk/64/000000/web-design.png' alt="Icon linking to Tran's portfolio website" height='30' />
         </a>
-        <a href='https://github.com/trnle/radiant' target='_blank' rel='noreferrer'>
+        <a href='https://github.com/trnle/radiant' target='_blank' rel='noopener noreferrer'>
           <img src='https://img.icons8.com/dusk/64/000000/github.png' alt="Icon linking to Tran's GitHub" height='30' />
         </a>
-        <a href='https://www.linkedin.com/in/trnle/' target='_blank' rel='noreferrer'>
+        <a href='https://www.linkedin.com/in/trnle/' target='_blank' rel='noopener noreferrer'>
           <img src='https://img.icons8.com/dusk/64/000000/linkedin.png' alt="Icon linking to Tran's LinkedIn" height='30' />
         </a>
       </div>
